f6f634204059261ecb3889ff3e7a496fbf422df7
PSToan016 <92621890+PSToan016@users.noreply.github.com>
2023-05-18 15:42:22 +0700

    Xong giao dien About

--- Nhom1_ASM_Java5/src/main/java/com/poly/controller/OkController.java
+++ Nhom1_ASM_Java5/src/main/java/com/poly/controller/OkController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OkController {
-	@RequestMapping("/index")
+	@RequestMapping("/about")
 	public String index() {
 		//Gọi file index
-		return "index";
+		return "about";
 	}
 }
 
